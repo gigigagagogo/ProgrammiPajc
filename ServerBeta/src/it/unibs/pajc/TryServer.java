b9fe09c5bc200ca41bbff30da205a1fb3c6bb732
gigigagagogo <robertbararu2@gmail.com>
2023-11-12 18:25:03 +0100

    commit di non so cosa

--- ServerBeta/src/it/unibs/pajc/TryServer.java
+++ ServerBeta/src/it/unibs/pajc/TryServer.java
@@ -12,6 +12,7 @@ public class TryServer {
 	public static void main(String[] args) {
 		int port=1234;
 		System.out.println("In attesa di un client....");
+		
 		try(
 			ServerSocket server= new ServerSocket(port);
 			Socket client = server.accept();
@@ -22,6 +23,16 @@ public class TryServer {
 					client.getInetAddress(),server.getInetAddress(),server.getLocalPort());
 			out.print("\n\nBenvenuto\n\n");
 			
+			String request;
+			
+			while((request=in.readLine()) != null) {
+				System.out.printf("\nrequest: %s\n", request);
+				if("@QUIT".equals(request.toUpperCase())) {
+					out.print("Torna presto");
+					break;
+				}
+				out.println(request.toUpperCase());
+			}
 			
 		}catch(IOException e) {
 			e.printStackTrace();
